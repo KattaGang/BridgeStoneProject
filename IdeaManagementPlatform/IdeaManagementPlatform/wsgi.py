3f709045304f5dfe41bb67a600c146f8faeb3329
Jay7221 <jns29072003@gmail.com>
2023-01-10 15:15:23 +0530

    Added Commit for hosting

--- IdeaManagementPlatform/IdeaManagementPlatform/wsgi.py
+++ IdeaManagementPlatform/IdeaManagementPlatform/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IdeaManagementPlatform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IdeaManagementPlatform.IdeaManagementPlatform.settings')
 
 application = get_wsgi_application()
